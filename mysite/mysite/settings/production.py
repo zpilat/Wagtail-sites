befda7d9f7b1d3242c954aa515d7d14f623b2287
Zdenek Pilat <zdenek.pilat@seznam.cz>
2024-12-18 19:15:09 +0000

    nové položky v receptech

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['zpilat.pythonanywhere.com']
+ALLOWED_HOSTS = ['pilatovic.pythonanywhere.com', 'localhost']
 
 try:
     from .local import *
