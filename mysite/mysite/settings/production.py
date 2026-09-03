f17759b25d7dc8dd9ad9ec1c72d784f6622fa4e7
zpilat <zdenek.pilat@seznam.cz>
2026-09-03 20:18:16 +0200

    Revert "fix: update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS for production settings"
    This reverts commit 42e080c53e20abccd912dd96341a2b87c2dc66ee.

--- mysite/mysite/settings/production.py
+++ mysite/mysite/settings/production.py
@@ -5,19 +5,8 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 DEBUG = False
 
-ALLOWED_HOSTS = [
-	"mysite-9293.rostiapp.cz",
-	"pilatovic.cz",
-	"www.pilatovic.cz",
-]
-CSRF_TRUSTED_ORIGINS = [
-    "https://www.pilatovic.cz",
-    "https://pilatovic.cz",
-]
+ALLOWED_HOSTS = ['pilatovic.pythonanywhere.com', 'localhost']
 
-WAGTAILADMIN_BASE_URL = "https://www.pilatovic.cz"
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 try:
     from .local import *
 except ImportError:
